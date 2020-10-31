119d6fc426ca79143dd6e84097cf1de2c536cee8
Carlos <60367213+RivasCVA@users.noreply.github.com>
2020-10-31 15:25:12 -0500

    Frontend UI improvements (#42)

--- frontend/src/Landing.js
+++ frontend/src/Landing.js
@@ -20,20 +20,21 @@ function Landing() {
                 <div>
                 <h1 style={{color: "#50FA7B", fontFamily: 'Alegreya Sans'}}><img src="/sea-snail.png" />AlgoShell</h1>
                 <p style={{color: "white"}}>Shell shock the coding interviews</p>
-                <button class="btn-lg" style={{backgroundColor: "#50FA7B"}}>Start Coding</button>
+                <button class="btn-lg">Start Coding</button>
                 </div>
             </div>
 
             <div id="under-main" style={{backgroundColor:"#282A36", color: "white", height:"720px"}}>
                 <div>
-                <p><em>It's like LeetCode except it doesn't require the internet.</em></p>
-                <iframe width="300" height="500" src="https://www.youtube.com/embed/k-famToMuqY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <p><em>What is AlgoShell?</em></p>
+                <iframe width="282" height="500" src="https://www.youtube.com/embed/HNIqKE6ITFs" frameborder="0" allow="accelerometer; autoplay; mute; loop; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
 
             <div id="footer" style={{backgroundColor: "#373A59", height: "120px"}}>
                 <div>
                 <p><span style={{color: "white"}}><img src="/sea-snail.png" />AlgoShell</span> <span style={{color: "#50FA7B"}}>Version 1.0</span></p>
+                <p><a class="contribute-link" href="https://github.com/andrewroblesdev/AlgoShell" target="_blank" style={{color: "#50FA7B"}}>Want to help?</a></p>
                 </div>
             </div>
             
